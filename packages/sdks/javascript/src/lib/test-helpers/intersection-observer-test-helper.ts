1b84637da70451ee0974b6ca00a0014de353418e
maroun-baydoun <maroun@ninetailed.com>
2024-03-21 15:25:12 +0100

    chore: fix missing type

--- packages/sdks/javascript/src/lib/test-helpers/intersection-observer-test-helper.ts
+++ packages/sdks/javascript/src/lib/test-helpers/intersection-observer-test-helper.ts
@@ -8,27 +8,29 @@ const instanceMap = new Map();
 beforeEach(() => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  global.IntersectionObserver = jest.fn((cb, options = {}) => {
-    const instance = {
-      thresholds: Array.isArray(options.threshold)
-        ? options.threshold
-        : [options.threshold],
-      root: options.root,
-      rootMargin: options.rootMargin,
-      observe: jest.fn((element: Element) => {
-        instanceMap.set(element, instance);
-        observerMap.set(element, cb);
-      }),
+  global.IntersectionObserver = jest.fn(
+    (cb, options: Partial<IntersectionObserverInit> = {}) => {
+      const instance = {
+        thresholds: Array.isArray(options.threshold)
+          ? options.threshold
+          : [options.threshold],
+        root: options.root,
+        rootMargin: options.rootMargin,
+        observe: jest.fn((element: Element) => {
+          instanceMap.set(element, instance);
+          observerMap.set(element, cb);
+        }),
 
-      unobserve: jest.fn((element: Element) => {
-        instanceMap.delete(element);
-        observerMap.delete(element);
-      }),
+        unobserve: jest.fn((element: Element) => {
+          instanceMap.delete(element);
+          observerMap.delete(element);
+        }),
 
-      disconnect: jest.fn(),
-    };
-    return instance;
-  });
+        disconnect: jest.fn(),
+      };
+      return instance;
+    }
+  );
 });
 
 afterEach(() => {
