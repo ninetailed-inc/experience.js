8cd2cda861668f4760207ca1a09896e03b96552b
Alexander Braunreuther <alexander.braunreuther@googlemail.com>
2024-06-03 19:38:00 +0200

    feat: move package version env var into lib

--- packages/sdks/shared/rollup.config.cjs
+++ packages/sdks/shared/rollup.config.cjs
@@ -1,24 +1,6 @@
 const nrwlConfig = require('@nx/react/plugins/bundle-rollup');
-const replace = require('@rollup/plugin-replace');
-require('dotenv').config({ path: '../../../.env.local' });
+const withPackageVersion = require('../../../withPackageVersion');
 
 module.exports = (config) => {
-  if (!process.env.NX_PACKAGE_VERSION) {
-    throw new Error('NX_PACKAGE_VERSION is not set');
-  }
-
-  const nxConfig = nrwlConfig(config);
-  return {
-    ...nxConfig,
-    plugins: [
-      ...nxConfig.plugins,
-      replace({
-        "process.env['NX_PACKAGE_VERSION']": JSON.stringify(
-          process.env.NX_PACKAGE_VERSION
-        ),
-        delimiters: ['', ''],
-        preventAssignment: true,
-      }),
-    ],
-  };
+  return withPackageVersion(nrwlConfig(config));
 };
