2bfe59ddb3ae30938519a6b2ea620cdd7bd7bdb8
Francois Le Pape <francois@ninetailed.com>
2024-08-14 15:32:45 +0200

    chore: export NinetailedInsightsApiClient from @ninetailed/experience.js-plugin-insights

--- packages/plugins/insights/src/index.ts
+++ packages/plugins/insights/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/NinetailedInsightsPlugin';
+export { NinetailedInsightsApiClient } from './lib/api/NinetailedInsightsApiClient';
 export { NinetailedInsightsPlugin as default } from './lib/NinetailedInsightsPlugin';
