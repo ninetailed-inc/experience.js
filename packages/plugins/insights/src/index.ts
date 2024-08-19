c8d6cb18eb86b9612e7e29e4fbb4efefc0242c8a
Francois Le Pape <francois@ninetailed.com>
2024-08-19 10:33:10 +0200

    chore: export type ComponentViewEventBatch from plugin-insights
    needed to use sendEventBatches()

--- packages/plugins/insights/src/index.ts
+++ packages/plugins/insights/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/NinetailedInsightsPlugin';
 export { NinetailedInsightsApiClient } from './lib/api/NinetailedInsightsApiClient';
 export { NinetailedInsightsPlugin as default } from './lib/NinetailedInsightsPlugin';
+export { type ComponentViewEventBatch } from './lib/types/Event/ComponentViewEventBatch';
