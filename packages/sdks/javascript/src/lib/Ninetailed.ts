a12a99131692eeb2b24684c1eba24b707b92af81
Alexander Braunreuther <alexander.braunreuther@googlemail.com>
2024-09-10 18:01:08 +0200

    refactor: move profile state handling into a state Manager

--- packages/sdks/javascript/src/lib/Ninetailed.ts
+++ packages/sdks/javascript/src/lib/Ninetailed.ts
@@ -64,6 +64,7 @@ import {
   hasComponentViewTrackingThreshold,
 } from '@ninetailed/experience.js-plugin-analytics';
 import { EventBuilder } from './utils/EventBuilder';
+import { ProfileStateManager } from './ProfileStateManager';
 
 declare global {
   interface Window {
@@ -137,6 +138,7 @@ export class Ninetailed implements NinetailedInstance {
   private readonly ninetailedCorePlugin: NinetailedCorePlugin;
   private readonly elementSeenObserver: ElementSeenObserver;
   private readonly observedElements: WeakMap<Element, ObservedElementPayload[]>;
+  private readonly profileStateManager: ProfileStateManager;
 
   private readonly clientId;
   private readonly environment;
@@ -238,20 +240,19 @@ export class Ninetailed implements NinetailedInstance {
       ...(storageImpl ? { storage: storageImpl } : {}),
     }) as AnalyticsInstance;
 
+    this.profileStateManager = new ProfileStateManager(this.instance);
+
     const detachOnReadyListener = this.instance.on('ready', () => {
       this.isInitialized = true;
       logger.info('Ninetailed Experience.js SDK is completely initialized.');
       detachOnReadyListener();
     });
 
-    // put in private method
-    this.onProfileChange((profileState) => {
-      this._profileState = profileState;
-
+    this.profileStateManager.onProfileChange((profileState) => {
       if (typeof window !== 'undefined') {
         window.ninetailed = Object.assign({}, window.ninetailed, {
-          profile: this.profileState.profile,
-          experiences: this.profileState.experiences,
+          profile: profileState.profile,
+          experiences: profileState.experiences,
         });
       }
     });
@@ -548,27 +549,9 @@ export class Ninetailed implements NinetailedInstance {
   };
 
   public onProfileChange = (cb: OnProfileChangeCallback) => {
-    cb(this.profileState);
+    cb(this.profileStateManager.getProfileState());
 
-    return this.instance.on(PROFILE_CHANGE, ({ payload }) => {
-      if (payload.error) {
-        cb({
-          ...this._profileState,
-          status: 'error',
-          profile: payload.profile,
-          experiences: payload.experiences,
-          error: payload.error,
-        });
-      } else {
-        cb({
-          ...this._profileState,
-          status: 'success',
-          profile: payload.profile,
-          experiences: payload.experiences,
-          error: null,
-        });
-      }
-    });
+    return this.profileStateManager.onProfileChange(cb);
   };
 
   public onSelectVariant = <
@@ -880,7 +863,7 @@ export class Ninetailed implements NinetailedInstance {
         identify: this.identifyAsWindowHandler.bind(this),
         reset: this.reset.bind(this),
         debug: this.debug.bind(this),
-        profile: this.profileState.profile,
+        profile: this.profileStateManager.getProfileState().profile,
       });
     }
   }
