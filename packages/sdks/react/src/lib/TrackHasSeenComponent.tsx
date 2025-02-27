0e5fd6257f9b95866b6cfcc899af9cadf8f11b4a
maroun-baydoun <maroun@ninetailed.com>
2025-02-27 18:03:15 +0100

    fix: remove react-intersection-observer

--- packages/sdks/react/src/lib/TrackHasSeenComponent.tsx
+++ packages/sdks/react/src/lib/TrackHasSeenComponent.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import { useInView } from 'react-intersection-observer';
+import React, { useEffect, useState, useRef } from 'react';
 import { isBrowser } from '@ninetailed/experience.js-shared';
 
 import { useNinetailed } from './useNinetailed';
@@ -15,7 +14,28 @@ export const TrackHasSeenComponent: React.FC<
   React.PropsWithChildren<TrackHasSeenComponentProps>
 > = ({ children, variant, audience, isPersonalized }) => {
   const ninetailed = useNinetailed();
-  const { ref, inView } = useInView({ triggerOnce: true });
+  const [inView, setInView] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) {
+        setInView(true);
+
+        // Disconnect the observer since we only want to track
+        // the first time the component is seen
+        observer.disconnect();
+      }
+    });
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
 
   useEffect(() => {
     if (isBrowser() && inView) {
