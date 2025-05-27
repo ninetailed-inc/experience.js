893116e26af0a0333c426744ff2f09ee32fcced1
Lotfi Arif <lotfi.arif@contentful.com>
2025-05-27 17:26:12 +0200

    feat: enhance Variable component to conditionally track flags and render styled output

--- packages/playgrounds/easy-hr/components/Variable/Variable.tsx
+++ packages/playgrounds/easy-hr/components/Variable/Variable.tsx
@@ -2,11 +2,37 @@ import { useFlag } from '@ninetailed/experience.js-react';
 import React from 'react';
 
 export const Variable: React.FC = () => {
-  const { value, status } = useFlag('colors', 'default');
+  const shouldTrack = () => {
+    // we can use this to conditionally track the variable
+    // e.g. based on user state, feature flags, etc.
+    return true;
+  };
+  const { value, status } = useFlag<{
+    padding: string;
+    color: string;
+  }>(
+    'testing-component-tracking',
+    {
+      padding: '10px',
+      color: 'blue',
+    },
+    shouldTrack
+  );
 
   // If still loading, return fallback
   if (status === 'loading') return <>{'fallback'}</>;
 
   // Otherwise, render the value directly
-  return <>{JSON.stringify(value)}</>;
+  return (
+    <>
+      <h1
+        style={{
+          padding: value.padding,
+          color: value.color,
+        }}
+      >
+        Variable Component
+      </h1>
+    </>
+  );
 };
