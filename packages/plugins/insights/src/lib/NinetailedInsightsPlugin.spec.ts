47e4cebdb387332036763bcc9b9ad7ef9bd2322b
maxrawlinger <max.rawlinger@googlemail.com>
2025-11-27 17:48:52 +0100

    test: add comments and minor updates to insights plugin spec

--- packages/plugins/insights/src/lib/NinetailedInsightsPlugin.spec.ts
+++ packages/plugins/insights/src/lib/NinetailedInsightsPlugin.spec.ts
@@ -4,10 +4,7 @@ import { NinetailedPlugin } from '@ninetailed/experience.js-plugin-analytics';
 import { Ninetailed } from '@ninetailed/experience.js';
 
 import { NinetailedInsightsPlugin } from './NinetailedInsightsPlugin';
-import {
-  getObserverOf,
-  intersect,
-} from './test-helpers/intersection-observer-test-helper';
+import { intersect } from './test-helpers/intersection-observer-test-helper';
 
 const insightsApiClientSendEventBatchesMock = jest.fn().mockResolvedValue({});
 
@@ -19,12 +16,14 @@ jest.mock('./api/NinetailedInsightsApiClient', () => {
   };
 });
 
+// TODO we need to refactor the tests to not rely on the insightsApiClientSendEventBatchesMock as the only way to verify if events were sent or not.
+// Because of the batching and queueing logic it is possible that multiple calls to observeElement result in no events being sent if they are considered duplicates or the queue is not full enough to be flushed.
 describe('NinetailedInsightsPlugin', () => {
   beforeEach(() => {
     insightsApiClientSendEventBatchesMock.mockClear();
   });
 
-  const mockProfile = (plugins: NinetailedPlugin[] = []) => {
+  const setupNinetailedInstance = (plugins: NinetailedPlugin[] = []) => {
     const apiClient = new NinetailedApiClient({ clientId: 'test' });
     apiClient.upsertProfile = jest.fn().mockResolvedValue({
       id: 'test',
@@ -37,14 +36,13 @@ describe('NinetailedInsightsPlugin', () => {
         id: 'p-a12b3c4d5e6f7g8h9i0j',
       },
     });
-    const ninetailed = new Ninetailed(apiClient, { plugins });
 
-    return { apiClient, ninetailed };
+    return new Ninetailed(apiClient, { plugins });
   };
 
   it('should send component events once the queue can be flushed', async () => {
     const insightsPlugin = new NinetailedInsightsPlugin();
-    const { ninetailed } = mockProfile([insightsPlugin]);
+    const ninetailed = setupNinetailedInstance([insightsPlugin]);
 
     insightsPlugin.setCredentials({
       clientId: 'test',
@@ -57,8 +55,6 @@ describe('NinetailedInsightsPlugin', () => {
 
     for (let i = 0; i < 25; i++) {
       const element = document.body.appendChild(document.createElement('div'));
-
-      getObserverOf(element);
 
       ninetailed.observeElement({
         element,
@@ -95,8 +91,7 @@ describe('NinetailedInsightsPlugin', () => {
 
   it('should not send component events when the queue is not ready to be flushed', async () => {
     const insightsPlugin = new NinetailedInsightsPlugin();
-    const { ninetailed } = mockProfile([insightsPlugin]);
-
+    const ninetailed = setupNinetailedInstance([insightsPlugin]);
     insightsPlugin.setCredentials({
       clientId: 'test',
       environment: 'development',
@@ -108,8 +103,6 @@ describe('NinetailedInsightsPlugin', () => {
 
     for (let i = 0; i < 5; i++) {
       const element = document.body.appendChild(document.createElement('div'));
-
-      getObserverOf(element);
 
       ninetailed.observeElement({
         element,
@@ -128,9 +121,10 @@ describe('NinetailedInsightsPlugin', () => {
     expect(insightsApiClientSendEventBatchesMock).not.toBeCalled();
   });
 
+  // This is a rare case and would only happen if a profile changes while the user is on the same page and the component would change e.g. from baseline to variant 1
   it('should track the same element with different payloads', async () => {
     const insightsPlugin = new NinetailedInsightsPlugin();
-    const { ninetailed } = mockProfile([insightsPlugin]);
+    const ninetailed = setupNinetailedInstance([insightsPlugin]);
 
     insightsPlugin.setCredentials({
       clientId: 'test',
@@ -142,8 +136,10 @@ describe('NinetailedInsightsPlugin', () => {
     jest.useFakeTimers();
 
     const element = document.body.appendChild(document.createElement('div'));
-    getObserverOf(element);
 
+    // TODO improve test to actually unobserve -> intersect -> observe again
+    // The queue needs to be flushed before unobserving. When unobserving before flushing, the event is lost.
+    // Generally, on a profile change triggers a flush of the queue afterwards the element will be unobserved as the element changes.
     for (let i = 0; i < 25; i++) {
       ninetailed.observeElement({
         element,
@@ -151,7 +147,6 @@ describe('NinetailedInsightsPlugin', () => {
         variantIndex: i,
       });
     }
-
     intersect(element, true);
 
     jest.runAllTimers();
@@ -164,7 +159,7 @@ describe('NinetailedInsightsPlugin', () => {
 
   it('should not track the same element with the same payload', async () => {
     const insightsPlugin = new NinetailedInsightsPlugin();
-    const { ninetailed } = mockProfile([insightsPlugin]);
+    const ninetailed = setupNinetailedInstance([insightsPlugin]);
 
     insightsPlugin.setCredentials({
       clientId: 'test',
@@ -176,7 +171,6 @@ describe('NinetailedInsightsPlugin', () => {
     jest.useFakeTimers();
 
     const element = document.body.appendChild(document.createElement('div'));
-    getObserverOf(element);
 
     for (let i = 0; i < 25; i++) {
       ninetailed.observeElement({
@@ -196,9 +190,10 @@ describe('NinetailedInsightsPlugin', () => {
     expect(insightsApiClientSendEventBatchesMock).toBeCalledTimes(0);
   });
 
+  // TODO We need to refactor the test as the insightsApiClientSendEventBatchesMock cannot be relied on here to check if the same element with same payload was sent multiple times or not
   it('should not track the same element with the same payload on multiple views', async () => {
     const insightsPlugin = new NinetailedInsightsPlugin();
-    const { ninetailed } = mockProfile([insightsPlugin]);
+    const ninetailed = setupNinetailedInstance([insightsPlugin]);
 
     insightsPlugin.setCredentials({
       clientId: 'test',
@@ -210,7 +205,6 @@ describe('NinetailedInsightsPlugin', () => {
     jest.useFakeTimers();
 
     const element = document.body.appendChild(document.createElement('div'));
-    getObserverOf(element);
 
     for (let i = 0; i < 25; i++) {
       ninetailed.observeElement({
@@ -229,15 +223,7 @@ describe('NinetailedInsightsPlugin', () => {
 
     await sleep(5);
 
-    const events = (insightsPlugin as any).events || [];
-    expect(events).toHaveLength(1);
-
     // Should not flush because only one unique event
     expect(insightsApiClientSendEventBatchesMock).toBeCalledTimes(0);
-
-    const seenElements =
-      (insightsPlugin as any).seenElements.get(element) || [];
-
-    expect(seenElements).toHaveLength(1);
   });
 });
