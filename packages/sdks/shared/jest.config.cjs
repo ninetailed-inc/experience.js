13dd9659d5a75c97e5935a68c4ab2ec4f3fc57f5
Francois Le Pape <32224751+Lp-Francois@users.noreply.github.com>
2026-02-09 15:47:12 +0100

    chore: update jest configuration to target ES2022

--- packages/sdks/shared/jest.config.cjs
+++ packages/sdks/shared/jest.config.cjs
@@ -2,7 +2,14 @@ module.exports = {
   displayName: 'sdks-shared',
   preset: '../../../jest.preset.js',
   transform: {
-    '^.+\\.[tj]s$': '@swc/jest',
+    '^.+\\.[tj]s$': [
+      '@swc/jest',
+      {
+        jsc: {
+          target: 'es2022',
+        },
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../../coverage/packages/sdks/shared',
